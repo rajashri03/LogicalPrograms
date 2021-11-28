6c6c4e5be409ff6b06ce1d751f7c97acd7225d4b
rajashri03 <telvekar.rajashri@gmail.com>
2021-11-28 20:33:33 +0530

    Perfect Number

---
+++ LogicalPrograms/LogicalPrograms/PerfectNum.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    public class PerfectNum
+    {
+    int Num,sum=0;
+    public void CheckPerfect()
+    {
+        Console.Write("Enter Number:");
+        Num = Convert.ToInt32(Console.ReadLine());
+        for (int i = 1; i <Num; i++)
+        {
+            if (Num % i == 0)
+            {
+               
+                Console.Write(i + " ");
+                sum = sum + i;
+            }
+
+        }
+       if(sum==Num)
+        {
+            Console.Write("\n"+ Num + " Is Perfect Number\n");
+
+        }
+        else
+        {
+            Console.Write("\n" + Num + " Is Not Perfect Number\n");
+        }
+    
+}
+ 
+    }
+
--- LogicalPrograms/LogicalPrograms/Program.cs
+++ LogicalPrograms/LogicalPrograms/Program.cs
@@ -4,6 +4,23 @@ public class Logical
     public static void Main(string[] args)
     {
         FibonacciSeries fibo = new FibonacciSeries();
-        fibo.FiboSeries();
+       
+        PerfectNum perfect = new PerfectNum();
+       
+        Console.Write("Select Number:\n1)FibonacciSeries\n2)PerfectNumber\n\n");
+        int option = Convert.ToInt32(Console.ReadLine());
+        switch(option)
+        {
+            case 1:
+                fibo.FiboSeries();
+                break;
+            case 2:
+                perfect.CheckPerfect();
+                break;
+            default:
+                Console.Write("Please Select Correct Number");
+                break;
+
+        }
     }
 }
