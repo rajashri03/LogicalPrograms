17eb0b300b64f79d54c2c59ee9da3b1ec8515d0c
rajashri03 <telvekar.rajashri@gmail.com>
2021-11-30 17:49:29 +0530

    Prime Number

---
+++ LogicalPrograms/LogicalPrograms/Prime.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    public class Prime
+    {
+    int Num, checkprime = 1;
+    public void Checkprime()
+    {
+        Console.Write("Enter Number To Check Number is prime or not:");
+        Num = Convert.ToInt16(Console.ReadLine());
+        for (int i=2;i<=Num/2;i++)
+        {
+            if(Num % i == 0)
+            {
+                checkprime = 0;
+            }
+            
+        }
+        if (checkprime == 1)
+        {
+            Console.Write(Num + " Is a Prime Number\n\n");
+        }
+        else
+        {
+            Console.Write(Num + " Is Not Prime Number\n\n");
+        }
+    }
+}
+
--- LogicalPrograms/LogicalPrograms/Program.cs
+++ LogicalPrograms/LogicalPrograms/Program.cs
@@ -3,24 +3,38 @@ public class Logical
 {
     public static void Main(string[] args)
     {
+        //Fibonacci Series
         FibonacciSeries fibo = new FibonacciSeries();
-       
-        PerfectNum perfect = new PerfectNum();
-       
-        Console.Write("Select Number:\n1)FibonacciSeries\n2)PerfectNumber\n\n");
-        int option = Convert.ToInt32(Console.ReadLine());
-        switch(option)
-        {
-            case 1:
-                fibo.FiboSeries();
-                break;
-            case 2:
-                perfect.CheckPerfect();
-                break;
-            default:
-                Console.Write("Please Select Correct Number");
-                break;
 
+        //Perfect Number
+        PerfectNum perfect = new PerfectNum();
+
+        //Prime Number
+        Prime prime = new Prime();
+
+      
+        bool check = true;
+        while (check)
+        {
+            Console.Write("Select Number:\n1)Fibonacci Series\n2)Perfect Number\n3)Prime Number\n\n");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    fibo.FiboSeries();
+                    break;
+                case 2:
+                    perfect.CheckPerfect();
+                    break;
+                case 3:
+                    prime.Checkprime();
+                    break;
+                default:
+                    check = !check;
+                    break;
+              
+
+            }
         }
     }
 }
